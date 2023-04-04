a502d66d42940809e387ae1b376e33e4ae825545
IOU4 <ouchaibimad000@gmail.com>
2023-04-04 10:53:12 +0000

    initial auth spring bootsrap

---
+++ backEnd/auth/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "ucode"
+version = "0.1"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
