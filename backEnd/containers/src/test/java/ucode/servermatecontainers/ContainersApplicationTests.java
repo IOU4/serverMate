2e7e8d4bf7b11ba404b398c7fd21aa577521a1c9
IOU4 <ouchaibimad000@gmail.com>
2023-04-06 09:56:52 +0000

    builerplate for containers micro-service

---
+++ backEnd/containers/src/test/java/ucode/servermatecontainers/ContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package ucode.servermatecontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
