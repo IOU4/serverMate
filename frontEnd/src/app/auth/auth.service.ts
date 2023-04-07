e76cbdbc88de51211999f709fe51dd046b711300
IOU4 <ouchaibimad000@gmail.com>
2023-04-07 10:16:43 +0000

    implemented auth service with login and register

--- frontEnd/src/app/auth/auth.service.ts
+++ frontEnd/src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  constructor() { }
+  baseApi: string = 'http://localhost:8080/auth/';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.http.post(this.baseApi + 'login', {
+      email: email,
+      password: password
+    });
+  }
+
+  register(email: string, username: string, password: string) {
+    return this.http.post(this.baseApi + 'register', {
+      email: email,
+      username: username,
+      password: password
+    });
+  }
 }
