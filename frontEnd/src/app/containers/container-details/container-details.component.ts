35501cd240342f400fab42ed6e5a19163a21e5e0
IOU4 <ouchaibimad000@gmail.com>
2023-04-09 12:10:57 +0000

    container details component

---
+++ frontEnd/src/app/containers/container-details/container-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-details',
+  templateUrl: './container-details.component.html',
+  styleUrls: ['./container-details.component.css']
+})
+export class ContainerDetailsComponent {
+
+}
