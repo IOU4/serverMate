c8bc004792be3134b739c9f7653628c1c8789482
IOU4 <ouchaibimad000@gmail.com>
2023-04-11 14:43:25 +0000

    deleted uncessary files

--- frontEnd/src/app/auth/login/login.component.ts
+++ frontEnd/src/app/auth/login/login.component.ts
@@ -7,7 +7,6 @@ import { AuthService, LoginResponse } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) { }
