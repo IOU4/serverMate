913babee157ac88cfba0b3b5abcd44323b78d6fb
IOU4 <ouchaibimad000@gmail.com>
2023-04-12 17:22:53 +0000

    added various method for container management

--- frontEnd/src/app/home/containers/containers.service.ts
+++ frontEnd/src/app/home/containers/containers.service.ts
@@ -1,16 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ContainerResponse, ConttainerLog } from './container';
 
-interface ContainerResponse {
-  Id: string
-  Names: string[]
-  Command: string
-  Created: number
-  Image: string
-  ImageID: string
-  Status: string
-  State: string
-}
 @Injectable({
   providedIn: 'root'
 })
@@ -20,6 +11,34 @@ export class ContainersService {
   private baseApiUrl = "http://localhost:8080/api/v1"
 
   getContainers() {
-    return this.http.get<ContainerResponse[]>(this.baseApiUrl + "/container");
+    return this.http.get<ContainerResponse[]>(`${this.baseApiUrl}/container`);
+  }
+
+  getContainerDetails(containerId: string) {
+    return this.http.get<ContainerResponse>(`${this.baseApiUrl}/container/${containerId}`);
+  }
+
+  getContainerLogs(containerId: string) {
+    return this.http.get<ConttainerLog>(`${this.baseApiUrl}/container/${containerId}/logs`);
+  }
+
+  stopContainer(containerId: string) {
+    return this.http.get(`${this.baseApiUrl}/container/${containerId}/stop`);
+  }
+
+  startContainer(containerId: string) {
+    return this.http.get(`${this.baseApiUrl}/container/${containerId}/start`);
+  }
+
+  restartContainer(containerId: string) {
+    return this.http.get(`${this.baseApiUrl}/container/${containerId}/restart`);
+  }
+
+  deleteContainer(containerId: string) {
+    return this.http.delete(`${this.baseApiUrl}/container/${containerId}`);
+  }
+
+  pruneContainers() {
+    return this.http.get(`${this.baseApiUrl}/container/prune`);
   }
 }
