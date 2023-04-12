58e8c0c6ba0229825e1bb17e8cf7a24cd13eedee
IOU4 <ouchaibimad000@gmail.com>
2023-04-12 17:20:59 +0000

    container details list updated

--- frontEnd/src/app/home/containers/container-details/container-details.component.ts
+++ frontEnd/src/app/home/containers/container-details/container-details.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContainerResponse } from '../container';
+import { ContainersService } from '../containers.service';
 
 @Component({
   selector: 'app-container-details',
   templateUrl: './container-details.component.html',
 })
-export class ContainerDetailsComponent {
+export class ContainerDetailsComponent implements OnInit {
+  constructor(private containerService: ContainersService, private route: ActivatedRoute) { }
 
+  container: ContainerResponse = {
+    Id: '',
+    Names: [],
+    Command: '',
+    Created: 0,
+    Image: '',
+    ImageID: '',
+    Status: '',
+    State: '',
+    Ports: [],
+    Mounts: [],
+    HostConfig: {
+      NetworkMode: ''
+    }
+  };
+  log: string = ''
+  ngOnInit(): void {
+    const containerId = this.route.snapshot.paramMap.get('id')!;
+    this.containerService.getContainerDetails(containerId).subscribe(container => {
+      this.container = container;
+    });
+    this.containerService.getContainerLogs(containerId).subscribe(log => {
+      this.log = log.log;
+    })
+  }
 }
