e79a38f101747445181585d8d0fb7ccbc2f22bf7
IOU4 <ouchaibimad000@gmail.com>
2023-04-12 17:22:37 +0000

    added route for container details

--- frontEnd/src/app/home/containers/containers-routing.module.ts
+++ frontEnd/src/app/home/containers/containers-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerDetailsComponent } from './container-details/container-details.component';
 import { ContainersListComponent } from './containers-list/containers-list.component';
 import { ContainersComponent } from './containers.component';
 
@@ -7,7 +8,8 @@ const routes: Routes = [
   {
     path: '', component: ContainersComponent, children: [
       { path: '', redirectTo: '/containers', pathMatch: 'full' },
-      { path: "containers", component: ContainersListComponent }
+      { path: "containers", component: ContainersListComponent },
+      { path: "containers/:id", component: ContainerDetailsComponent }
     ]
   }
 ];
