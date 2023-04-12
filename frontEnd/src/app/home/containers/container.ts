4e726e68d220f07a77e1d5a1d719254b70ed4742
IOU4 <ouchaibimad000@gmail.com>
2023-04-12 17:22:20 +0000

    take type to sperate file

--- frontEnd/src/app/home/containers/container.ts
+++ frontEnd/src/app/home/containers/container.ts
@@ -15,3 +15,39 @@ export enum ContainerState {
   DEAD = "dead",
   UKNOWN = "uknown"
 }
+
+export interface ContainerResponse {
+  Id: string
+  Names: string[]
+  Command: string
+  Created: number
+  Image: string
+  ImageID: string
+  Status: string
+  State: string
+  Ports: Port[]
+  Mounts: Mount[]
+  HostConfig: {
+    NetworkMode: string
+  }
+}
+
+export interface Port {
+  IP: string
+  PublicPort: number
+  PrivatePort: number
+  Type: string
+}
+
+export interface Mount {
+  Type: string
+  Name: string
+  Source: string
+  Destination: string
+  driver: string
+  Mode: string
+}
+
+export interface ConttainerLog {
+  log: string
+}
